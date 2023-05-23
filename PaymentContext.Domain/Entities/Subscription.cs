1c0a4e26ee83b8f819dd34198d3e76954f6ab08e
Igor Santiago <contato.igorsantiago@outlook.com>
2023-05-23 16:35:51 -0300

    Refactoring some parts of the code

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -2,10 +2,41 @@ namespace PaymentContext.Domain.Entities;
 
 public class Subscription
 {
-    public DateTime CreateDate { get; set; }
-    public DateTime LastUpdateDate { get; set; }
-    public DateTime? ExpireDate { get; set; }
-    public bool IsActive { get; set; }
+    private IList<Payment> _payments;
+    public Subscription(DateTime? expireDate)
+    {
+        CreateDate = DateTime.Now;
+        LastUpdateDate = DateTime.Now;
+        ExpireDate = expireDate;
+        _payments = new List<Payment>();
+    }
 
-    public List<Payment> Payments { get; set; }
+    public DateTime CreateDate { get; private set; }
+    public DateTime LastUpdateDate { get; private set; }
+    public DateTime? ExpireDate { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public IReadOnlyCollection<Payment> Payments
+    {
+        get
+        {
+            return _payments.ToArray();
+        }
+    }
+
+    public void AddPayment(Payment payment)
+    {
+        _payments.Add(payment);
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+        LastUpdateDate = DateTime.Now;
+    }
+    public void Inactivate()
+    {
+        IsActive = false;
+        LastUpdateDate = DateTime.Now;
+    }
 }
